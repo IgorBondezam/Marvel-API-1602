3482e552f116b9adb9b88b677cf9104796e48baa
Igor Bondezam França <igorbfraca@hotmail.com>
2024-04-21 02:33:08 -0300

    Adicionando restante dos métodos do controller de character

--- src/controller/character.controller.ts
+++ src/controller/character.controller.ts
@@ -8,6 +8,26 @@ class CharacterController{
         res.status(200).send();
     }
 
+    public async findByIdCharacter(req: Request, res: Response): Promise<void>{
+        res.json(await characterService.findById(req.params.id));
+        res.status(200).send();
+    
+
+    public async createCharacter(req: Request, res: Response): Promise<void>{
+        res.json(await characterService.create(req.body))
+        res.status(201).send();
+    }
+
+    public async updateCharacter(req: Request, res: Response): Promise<void>{
+        res.json(await characterService.update(req.params.id, req.body));
+        res.status(200).send();
+    }
+
+    public async deleteCharacter(req: Request, res: Response): Promise<void>{
+        res.json(await characterService.delete(req.params.id));
+        res.status(204).send();
+    }
+
 }
 
 export default new CharacterController();
